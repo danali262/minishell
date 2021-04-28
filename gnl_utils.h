39f4426120155b11642cbc41cf12819aa7272999
Moliie-S <olcha.olcha@gmail.com>
2021-04-28 16:52:56 +0200

    Cleanup.

--- gnl_utils.h
+++ gnl_utils.h
@@ -6,16 +6,17 @@
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/26 14:08:32 by osamara       #+#    #+#                 */
-/*   Updated: 2021/04/26 15:35:28 by osamara       ########   odam.nl         */
+/*   Updated: 2021/04/28 15:30:48 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GNL_UTILS_H
 # define GNL_UTILS_H
 
+#include "command_history.h"
 #include "line_state.h"
 
-int	read_command_line(int fd, t_line *line_state);
+int	read_command_line(int fd, t_history *history, t_line *line_state);
 
 
 #endif
--- parsing_utils.h
+++ parsing_utils.h
@@ -6,16 +6,13 @@
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/23 16:52:24 by osamara       #+#    #+#                 */
-/*   Updated: 2021/02/28 16:18:39 by osamara       ########   odam.nl         */
+/*   Updated: 2021/04/28 15:35:05 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_UTILS_H
 # define PARSING_UTILS_H
 
-int		are_valid_characters(char *line, int line_num);
-int		is_empty_line(char *line);
-int		has_identifier(char *line, char *identifier, int *identifier_len);
 int		is_valid_component(const char *string, int *component);
 char	**split_into_components(char *line, char separator, int num_components);
 
