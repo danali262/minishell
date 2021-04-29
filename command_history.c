791711c174256f0ac8201b04e6df66680f372094
Olga Samara <osamara@f0r4s2.codam.nl>
2021-04-29 12:48:27 +0200

    Implamanted iteration mode in history not to write buffer to history without newline being pressed.

--- command_history.c
+++ command_history.c
@@ -8,6 +8,7 @@ void	init_history(t_history *history)
 	ft_bzero(history->lines, MAX_HISTORY);
 	history->num_lines = 0;
 	history->last_shown_line = -1;
+	history->iter_mode = 0;
 }
 
 
@@ -23,4 +24,5 @@ void	free_history(t_history *history)
 	}
 	history->num_lines = 0;
 	history->last_shown_line = -1;
+	history->iter_mode = 0;
 }
--- command_history.h
+++ command_history.h
@@ -14,6 +14,7 @@ typedef struct s_history
 	char	*lines[MAX_HISTORY];
 	int		num_lines;
 	int		last_shown_line;
+	int		iter_mode;
 }				t_history;
 
 void	init_history(t_history *history);
