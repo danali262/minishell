bead6c786ca910ab9e99c9b4e22c6a38d20ac5db
Moliie-S <olcha.olcha@gmail.com>
2021-05-17 21:13:33 +0200

    Searching the filename path for execve().

---
+++ src/executor/executable_path.c
@@ -0,0 +1,84 @@
+#include "executor.h"
+
+#include "builtins/builtins.h"
+
+#include "libft.h"
+
+#include <unistd.h>
+#include <fcntl.h>
+
+static char	*append_cmd_to_dir(char *directory, char *cmd_alias)
+{
+	size_t	dir_len;
+	size_t	alias_len;
+	char	*cmd_path;
+
+	dir_len = ft_strlen(directory);
+	alias_len = ft_strlen(cmd_alias);
+	if (directory[dir_len] != '/')
+	{
+		cmd_path = malloc(sizeof(char) * (dir_len + alias_len) + 2);
+		ft_memmove(cmd_path, directory, dir_len);
+		cmd_path[dir_len] = '/';
+		ft_memmove(cmd_path + dir_len + 1, cmd_alias, alias_len);
+		cmd_path[dir_len + alias_len + 2] = '\0';
+	}
+	else
+		cmd_path = ft_strjoin(directory, cmd_alias);
+	return (cmd_path);
+}
+
+static int	is_valid_path(char *cmd_location)
+{
+	int	fd;
+
+	fd = open(cmd_location, O_RDONLY);
+	if (fd > 2)
+	{
+		return (1);
+	}
+	close(fd);
+	return (0);
+}
+
+static char	**get_directories_list()
+{
+	int			i;
+	int			j;
+	extern char **environ;
+	char		*path_str;
+
+	i = 0;
+	while (environ[i] != 0)
+	{
+		if (ft_strncmp(environ[i], "PATH", 4) == 0)
+            break ;
+		i++;
+	}
+	path_str = environ[i];
+	j = 0;
+	while (path_str[j] != '/')
+		j++;
+	return (ft_split(path_str + j, ':'));
+}
+
+char	*locate_executable_path(t_treenode *simple_cmd_node)
+{
+	char 		**dir_list;
+	char		*cmd_location;
+	int			i;
+
+    dir_list = get_directories_list();
+	i = 0;
+	cmd_location = NULL;
+	while (dir_list[i] != NULL)
+	{
+		cmd_location = append_cmd_to_dir(dir_list[i], simple_cmd_node->data);
+		if (is_valid_path(cmd_location))
+			break;
+		i++;
+	}
+	free_array_memory(dir_list);
+	return (cmd_location);
+}
+
