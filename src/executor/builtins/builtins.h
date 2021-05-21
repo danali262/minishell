40694597004e13a070be7c4c16ff9dee3d13ba58
Moliie-S <olcha.olcha@gmail.com>
2021-05-21 21:44:07 +0200

    functions replacement, cleanup

--- src/executor/builtins/builtins.h
+++ src/executor/builtins/builtins.h
@@ -4,8 +4,16 @@
 #include "shell_state.h"
 
 int	can_execute_builtin(t_treenode *syntax_tree, t_shell *shell);
-int	execute_echo(t_treenode *simple_cmd_node);
+int	execute_echo(t_treenode *simple_cmd_node, t_shell *shell);
 int	execute_exit(t_treenode *simple_cmd_node, t_shell *shell);
+int	execute_pwd(t_treenode *simple_cmd_node, t_shell *shell);
+int	execute_cd(t_treenode *simple_cmd_node, t_shell *shell);
+
+/*
+**	builtins_utilities:
+*/
+
+int		count_arguments(t_treenode *simple_cmd_node);
 
 
 #endif
