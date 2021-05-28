5757191e2e1a57984ebd174be0e2237ead23a9c1
Moliie-S <olcha.olcha@gmail.com>
2021-05-28 11:43:54 +0200

    Bugfix: environment list prints the last node now.

--- src/executor/builtins/env.c
+++ src/executor/builtins/env.c
@@ -9,7 +9,7 @@ int execute_env(t_treenode *simple_cmd_node, t_shell *shell)
 	{
 
 		node = shell->env_list; 
-		while (node->next != NULL)
+		while (node != NULL)
 		{
 			printf("%s=%s\n", node->name, node->value);
 			node = node->next;
