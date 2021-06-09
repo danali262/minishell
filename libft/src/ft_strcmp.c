2aa2769a2ec7f3965c6ec7495635030e22d31ef4
Moliie-S <olcha.olcha@gmail.com>
2021-06-09 16:35:41 +0200

    Added strcmp to libft.

---
+++ libft/src/ft_strcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strcmp.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: osamara <osamara@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/10/29 20:09:09 by osamara       #+#    #+#                 */
+/*   Updated: 2021/06/09 15:41:21 by osamara       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	unsigned int	index;
+	unsigned char	*ptr1;
+	unsigned char	*ptr2;
+
+	ptr1 = (unsigned char *)s1;
+	ptr2 = (unsigned char *)s2;
+	index = 0;
+	while ((*ptr1 != '\0' || *ptr2 != '\0') && *ptr1 == *ptr2)
+	{
+		ptr1++;
+		ptr2++;
+	}
+	return (*ptr1 - *ptr2);
+}
