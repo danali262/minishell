95dc8821ad874994079c69ebcf74d858de3ae63f
Moliie-S <olcha.olcha@gmail.com>
2021-06-19 23:11:01 +0200

    refactoring: removed unnecessary variables

--- src/executor/environment/dollar_question.c
+++ src/executor/environment/dollar_question.c
@@ -4,11 +4,8 @@
 
 char	*replace_dollar_question(t_shell *shell)
 {
-	char	*value;
 	char	*exit_code_str;
 
-	value = NULL;
 	exit_code_str = ft_itoa(shell->exit_code);
-	value = exit_code_str;
-	return (value);
+	return (exit_code_str);
 }
