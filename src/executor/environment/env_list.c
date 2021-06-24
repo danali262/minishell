63a0103fad8ab87fe5522c16122339775c893087
Moliie-S <olcha.olcha@gmail.com>
2021-06-24 12:37:49 +0200

    Bugfix: copying environment variable value into a new string.

--- src/executor/environment/env_list.c
+++ src/executor/environment/env_list.c
@@ -62,14 +62,19 @@ int	add_to_env_list(char *envar_name, char *envar_value, t_envlist	**env_list)
 int	update_env_list(t_shell *shell, char *envar_name, char *value_start)
 {
 	int			change_value_res;
+	char		*envar_value;
 	t_envlist	*envar_node;
 
+   envar_value = NULL;
     change_value_res = change_env_value(shell, envar_name, value_start);
 	if (change_value_res == ERROR)
 		return (ERROR);
 	else if (change_value_res == NOT_IN_ENVLIST)
 	{
-		envar_node = ft_env_lstnew(envar_name, value_start);
+		envar_value = ft_strdup(value_start);
+		if (envar_value == NULL)
+			return (ERROR);
+		envar_node = ft_env_lstnew(envar_name, envar_value);
 		if (!envar_node)
 			return (ERROR);
 		ft_env_lstadd_before_last_node(&(shell->env_list), envar_node);
