95580c5121bc3d868b1ca6db8a1d60d1d4343775
Moliie-S <olcha.olcha@gmail.com>
2021-06-28 14:12:14 +0200

    Bugfix: check if single quote is not found in the string containing  an environment variable.

--- src/executor/strip_quotes.c
+++ src/executor/strip_quotes.c
@@ -72,8 +72,10 @@ bool	is_wrapped_by_single_quotes(char *arg_string)
 	char	*dollar_sign_position;
 	char	*single_quote_position;
 
-	dollar_sign_position = ft_strchr(arg_string, '$');
 	single_quote_position = ft_strchr(arg_string, '\'');
+	dollar_sign_position = ft_strchr(arg_string, '$');
+	if (single_quote_position == NULL || dollar_sign_position == NULL)
+		return (false);
 	if (dollar_sign_position - single_quote_position > 0)
 		return (true);
 	return (false);
