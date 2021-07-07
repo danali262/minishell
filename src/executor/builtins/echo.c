21737aa91476533955d3337aa41aaa2fb420c897
Moliie-S <olcha.olcha@gmail.com>
2021-07-07 12:12:00 +0200

    Printing a newline to the stdout if the environment variable is not found in the env_list.

--- src/executor/builtins/echo.c
+++ src/executor/builtins/echo.c
@@ -22,15 +22,15 @@ int	execute_echo(t_treenode *simple_cmd_node, t_shell *shell)
 	arg_node = simple_cmd_node->left;
 	n_option = is_n_option(arg_node);
 	if (n_option)
-	{
-		shell->is_newline = false;
 		arg_node = arg_node->left;
-	}
 	while (arg_node != NULL)
 	{
 		arg_node->data = parse_argument_value(arg_node, shell);
 		if (arg_node->data == NULL)
+		{
+			ft_putstr_fd("\n", STDOUT_FILENO);
 			return (ERROR);
+		}
 		ft_putstr_fd(arg_node->data, STDOUT_FILENO);
 		if (arg_node->left != NULL && arg_node->type != NODE_VAR)
 			ft_putstr_fd(" ", STDOUT_FILENO);
